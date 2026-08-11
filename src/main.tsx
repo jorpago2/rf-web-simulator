a27384c01ec0ba7c4de2392ef058e24cb12371a2
Jorge Parra <297438018+jorpago2@users.noreply.github.com>
2026-08-11 17:45:41 +0200

    Adopt scientific UI provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { GlobalTheme } from '@carbon/react'
+import { ScientificUiProvider } from '@jorpago2/scientific-ui'
 import '@xyflow/react/dist/style.css'
 import './carbon.scss'
 import './index.css'
 import '@jorpago2/scientific-ui/styles.css'
 
-document.documentElement.classList.add('cds--g10')
 import App from './app/App'
 
 const root = document.getElementById('root')
@@ -14,6 +13,6 @@ if (!root) throw new Error('Application root element was not found.')
 
 createRoot(root).render(
   <StrictMode>
-    <GlobalTheme theme="g10"><App /></GlobalTheme>
+    <ScientificUiProvider><App /></ScientificUiProvider>
   </StrictMode>,
 )
